379ca3f872ca8c223e412506eb08edc4284e192f
Matt Simpson <matt.simpson3@gmail.com>
2013-08-10 13:14:57 -0500

    Add deployment to amazon

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,30 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application,       'coffeencoke.com'
+set :repository,        'public'
+set :scm,               :none
+set :deploy_via,        :copy
+set :copy_compression,  :gzip
+set :use_sudo,          false
+set :host,              '54.235.217.226'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :web,  host
+role :app,  host
+role :db,   host, :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user,    'matt'
+set :group,   user
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :deploy_to,    "/home/#{user}/apps/#{application}"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+before 'deploy:update', 'deploy:update_jekyll'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    %x(rm -rf _site/* && jekyll)
+  end
+end
